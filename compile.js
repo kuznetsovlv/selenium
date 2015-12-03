d3d4d20b1da408c3a39090149c226fe09cfbab22
leonid <kuznetsovlv1981@gmail.com>
2015-12-03 18:20:49 +0300

    compile.js: forCircle added

--- compile.js
+++ compile.js
@@ -53,19 +53,24 @@
 
 	document.body.appendChild(table);
 
-	function parseCommands (values) {
+	function parseCommands (values, vars) {
 		function _col (val) {
 			var td = document.createElement('td');
 			td.appendChild(document.createTextNode(val));
 			return td;
 		}
 		var fragment = document.createDocumentFragment();
-		for (var key in values)
-			values[key] = ('' + values[key]).split('<>');
+		for (var key in values) {
+			var tmp = ('' + values[key]).split('<>');
+			tmp.forEach(function (elem, i, arr) {
+				if (elem in vars)
+					arr[i] = vars[elem];
+			});
+			values[key] = tmp;
+		}
 		var commands = values.command || [''],
 		    expects = values.expect || [''];
 		values = values.value || [''];
-		
 		for (var c = 0, cl = commands.length; c < cl; ++c) {
 			var command = commands[c];
 			for (var v = 0, vl = values.length; v < vl; ++v) {
@@ -83,45 +88,100 @@
 	    i = 1,
 	    values,
 	    SETS = {},
-	    setMaking = null;
+	    setMaking = null,
+		forCircle = null,
+		circle = [];
 	conf.getHeader();
 
-	while (values = conf.getValues(i++)) {
+	function valuesToCommand (values, vars, strNum, endCircle) {
 		var command = values.command,
 		    value = values.value || '';
 		if (!command)
-			continue;
+			return;
 		if (SETS[command]) {
 			var set = SETS[command].cloneNode(true);
-			if (setMaking)
+			if (forCircle && !endCircle) {
+				var tmp = set.firstChild;
+				while (tmp && tmp.children) {
+					circle.push({
+						command: tmp.children[0].firstChild.data || '',
+						value: tmp.children[1].firstChild.data || '',
+						expect: tmp.children[2].firstChild.data || ''
+					});
+					tmp = tmp.nextSibling;
+				}
+			} else if (setMaking) {
 				SETS[setMaking].appendChild(set);
-			else
+			} else {
 				tbody.appendChild(set);
+			}
 		} else {
 			switch (command) {
 				case 'title': setTitle(value); break;
 				case 'href': link.setAttribute('href', value); break;
+				case 'forCircle':
+					if (forCircle)
+						throw "Str " + strNum + ": Incorrectstructure: trying start forCircle while not finished one.";
+					forCircle = [];
+					var r = /(?:\([^()]*\))/g,
+					    res;
+					while ((res = r.exec(value))) {
+						var tmp = res[0].split('<>');
+						tmp[0] = tmp[0].substr(1);
+						tmp[tmp.length - 1] = tmp[tmp.length - 1].substr(0, tmp[tmp.length - 1].length - 1);
+						forCircle.push(tmp);
+					}
+					break;
+				case 'endForCircle':
+					if (!forCircle)
+						throw "Str " + strNum + ": Incorrect structure: trying finish unstarted forCircle.";
+					var varList = forCircle[0],
+					    valength = varList.length,
+					    vars = {};
+					for (var i = 1, f = forCircle.length; i < f; ++i) {
+						var elem = forCircle[i];
+						for (var j = 0; j < valength; ++j)
+							vars[varList[j]] = elem[j];
+						for (var j = 0, l = circle.length; j < l; ++j) {
+							var tmp = circle[j],
+							    elem = {};
+							for (var key in tmp)
+								elem[key] = tmp[key];
+							valuesToCommand(elem, vars, strNum, true);
+						}
+					}
+					forCircle = null;
+					circle = [];
+					break;
 				case 'startSet':
 					if (setMaking)
-						throw "Str " + i + ": Incorrect structure: trying start set while not finished one.";
+						throw "Str " + strNum + ": Incorrect structure: trying start set while not finished one.";
 					if (SETS[value])
-						throw "Str " + i + ": Set name " + value + " already exists.";
+						throw "Str " + strNum + ": Set name " + value + " already exists.";
 					setMaking = value;
 					SETS[value] = document.createDocumentFragment();
 					break;
 				case 'endSet':
 					if (!setMaking)
-						throw "Str " + i + ": Incorrect structure: trying finish unstarted set.";
+						throw "Str " + strNum + ": Incorrect structure: trying finish unstarted set.";
 					setMaking = null;
 					break;
 				default:
-					values = parseCommands(values);
-					if (setMaking)
-						SETS[setMaking].appendChild(values);
-					else
-						tbody.appendChild(values);
+					if(forCircle && !endCircle) {
+						circle.push(values);
+					} else {
+					values = parseCommands(values, vars);
+						if (setMaking)
+							SETS[setMaking].appendChild(values); 
+						else
+							tbody.appendChild(values);
+					}
 			}
 		}
+	}
+
+	while (values = conf.getValues(i)) {
+			valuesToCommand(values, {}, i++, false);
 	}
 	document.drawDocument([path.replace(/\.\w*/, ''), 'html'].join('.'), 'selenium', true);
 })()
